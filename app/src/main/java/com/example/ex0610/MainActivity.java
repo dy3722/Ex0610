d26f1467a8c655ebadd43dcbc094235f49366dbd
dy3722 <dy3722@bs.amalnet.k12.il>
2025-03-31 13:39:16 +0300

    end design

--- app/src/main/java/com/example/ex0610/MainActivity.java
+++ app/src/main/java/com/example/ex0610/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ex0610;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +15,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    LinearLayout main;
+    Switch sw;
+    ToggleButton tb;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main = findViewById(R.id.main);
+        sw = findViewById(R.id.sw);
+        tb = findViewById(R.id.tb);
     }
 
+    public void click(View view) {
 
+    }
 }
